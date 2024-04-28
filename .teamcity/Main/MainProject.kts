47d18d9c22dff4b3a44e3e35692e7cfafcd7a6ab
jensstockhausen <jens@famst.de>
2024-04-28 16:19:44 +0200

    setup chain

--- .teamcity/Main/MainProject.kts
+++ .teamcity/Main/MainProject.kts
@@ -18,41 +18,70 @@ class ProjectChain(branch : String) : Project({
     name = "MainProject on $branch"
     id("MainProject_On_$branch")
 
-    val bts = sequential {
+    val buildCheckSystem = BuildCheckSystem(branch = branch)
 
-        buildType(BuildCheckSystem(branch = branch))
+    val buildCompileProjectA = BuildCompileProject(projectName = "PrjA", branch = branch)
+    val buildCompileProjectB = BuildCompileProject(projectName = "PrjB", branch = branch)
 
-        parallel {
+    val buildRunTestsA = BuildRunTests(projectName = "PrjA", branch = branch)
+    val buildRunTestsB = BuildRunTests(projectName = "PrjB", branch = branch)
+    val buildRunTestsAB = BuildRunTests(projectName = "Integ_PrjA_PrjB", branch = branch)
 
-            sequential {
+    buildType(buildCheckSystem)
 
-                val c = BuildCompileProject(projectName = "PrjA", branch = branch)
-                val t = BuildRunTests(projectName = "PrjA", branch = branch)
+    subProject {
+        name = "Sub Compile on $branch"
+        id("Sub_Compile_On_$branch")
+        buildType(buildCompileProjectA)
+        buildType(buildCompileProjectB)
+    }
 
-                t.dependencies {
-                    artifacts(c){
-                        artifactRules = "*.txt => ./"
-                        cleanDestination = true
-                    }
-                }
+    subProject {
+        name = "Sub Test on $branch"
+        id("Sub_Test_On_$branch")
+        buildType(buildRunTestsA)
+        buildType(buildRunTestsB)
+        buildType(buildRunTestsAB)
+    }
 
 
-                buildType(c)
-                buildType(t)
-            }
-
-            sequential {
-                buildType(BuildCompileProject(projectName = "PrjB", branch = branch))
-                buildType(BuildRunTests(projectName = "PrjB", branch = branch))
-            }
-
+    buildRunTestsA.dependencies {
+        artifacts(buildCompileProjectA){
+            artifactRules = "*.txt => ./"
+            cleanDestination = true
         }
+    }
 
-        buildType(BuildRunTests(projectName = "Integ_PrjA_PrjB", branch = branch))
+    buildRunTestsB.dependencies {
+        artifacts(buildCompileProjectB){
+            artifactRules = "*.txt => ./"
+            cleanDestination = true
+        }
+    }
 
+
+    val bts = sequential {
+        buildType(buildCheckSystem)
+        parallel {
+            sequential {
+                buildType(buildCompileProjectA)
+                buildType(buildRunTestsA)
+            }
+            sequential {
+                buildType(buildCompileProjectB)
+                buildType(buildRunTestsB)
+            }
+        }
+        buildType(buildRunTestsAB)
     }.buildTypes()
 
-    bts.forEach { bt -> buildType(bt)}
+    //bts.forEach { bt -> buildType(bt)}
+
+    buildTypesOrder = arrayListOf(
+        buildCheckSystem,
+        buildCompileProjectA, buildCompileProjectA,
+        buildCompileProjectB, buildCompileProjectB,
+        buildRunTestsAB)
 
 })
 
